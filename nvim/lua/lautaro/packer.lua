a79f85383a2233ef9fed3fb30b49ccb313a3f9fe
Lautaro Acosta <acostaquintanalautaro@gmail.com>
2022-11-27 13:44:53 -0300

    Segunda versión

--- nvim/lua/lautaro/packer.lua
+++ nvim/lua/lautaro/packer.lua
@@ -4,58 +4,61 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'lervag/vimtex'
-  use 'sainnhe/gruvbox-material'
-  use {
+    -- Package Manager
+    use 'wbthomason/packer.nvim'
+    
+    -- Plugin para Latex
+    use 'lervag/vimtex'
+    
+    -- Tema de color
+    use 'sainnhe/gruvbox-material'
+    
+    -- Plugin para configurar la apariencia del editor
+    use {
 	"windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
     }
-  use 'tpope/vim-commentary'
-  use "lukas-reineke/indent-blankline.nvim"
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'ryanoasis/vim-devicons'
-  use 'tiagofumo/vim-nerdtree-syntax-highlight'
-  use 'scrooloose/nerdtree'
-  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate",}
-  use 'p00f/nvim-ts-rainbow'
+    use 'tpope/vim-commentary'
+    use "lukas-reineke/indent-blankline.nvim"
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+    use 'ryanoasis/vim-devicons'
+    use 'p00f/nvim-ts-rainbow'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'ap/vim-css-color'
+    -- NERDTree  
+    use 'scrooloose/nerdtree'
+    use 'tiagofumo/vim-nerdtree-syntax-highlight'
+        
+    -- TreeSitter
+    use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate",}
 
-  -- use 'ms-jpq/coq_nvim'
-  -- use 'ms-jpq/coq.artifacts'
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  -- use 'mhinz/vim-startify'
-  use 'nvim-tree/nvim-web-devicons'
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
--- or                            , branch = '0.1.x',
+    -- Configuración para los servidores de lenguaje
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    
+    -- Telescope, File Finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
       requires = { {'nvim-lua/plenary.nvim'} }
-}
-
+    }
     local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    
 
-  use 'ap/vim-css-color'
-
-
-  -- use 'junegunn/goyo.vim'
-  -- use 'junegunn/limelight.vim'
-
- use 'nvim-telescope/telescope-media-files.nvim'
-
-
+    -- ToggleTerm, Terminal adentro de nvim
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+    end}
 end)
 
